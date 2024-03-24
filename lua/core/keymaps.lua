45e4aac10ca08bc7f1019dd207451825d04a8ab6
deltxprt <vincent.genest@protonmail.com>
2024-03-24 12:20:30 -0400

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use space for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.keymap.set('n', '<leader>un', ':u<CR>')
+vim.keymap.set('n', '<leader>re', ':redo<CR>')
+
+-- mouse settings
+vim.opt.mouse = ''
---
+++ lua/core/plugin_config/nvim-notify.lua
@@ -0,0 +1,1 @@
+require("nvim-notify").setup()
