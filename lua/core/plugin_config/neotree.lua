57eca1c9a2a58ee262e6a7a62a8082346fa5e310
deltxprt <vincent.genest@protonmail.com>
2026-03-01 20:21:56 -0500

    update

--- lua/core/plugin_config/neotree.lua
+++ lua/core/plugin_config/neotree.lua
@@ -1,6 +1,25 @@
 require('neo-tree').setup({
   -- options go here
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_hidden = false,
+    }
+  },
+  window = {
+    mappings = {
+      ["P"] = {
+        "toggle_preview",
+        config = {
+          use_float = false,
+          -- use_image_nvim = true,
+          -- use_snacks_image = true,
+          -- title = 'Neo-tree Preview',
+        },
+      },
+    }
+  }
 })
 
 
-vim.keymap.set('n', '<c-n>', ':Neotree filesystem toggle left<CR>')
+vim.keymap.set('n', '<c-n>', ':Neotree filesystem toggle float<CR>')
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -87,45 +87,9 @@ return {
     -- If you use nix, you can build from source using latest nightly rust with:
     -- build = 'nix run .#build-plugin',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
+    config = function()
+      require('core.plugin_config.blink')
+    end,
     opts_extend = { "sources.default" }
   },
 
@@ -186,6 +150,38 @@ return {
   {
     "simrat39/rust-tools.nvim",
     ft = "rust"
+  },
+
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
+
+      elixir.setup {
+        nextls = {enable = true},
+        elixirls = {
+          enable = true,
+          settings = elixirls.settings {
+            dialyzerEnabled = false,
+            enableTestLenses = false,
+          },
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          end,
+        },
+        projectionist = {
+          enable = true
+        }
+      }
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
 
   -- DAP
